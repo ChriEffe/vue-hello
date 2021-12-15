018c3d8f998d77b71f23f6e3489a04eada3bb5f3
ChriEffe <christianferrazzano93@gmail.com>
2021-12-15 14:44:37 +0100

    fine

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
 /*Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 Aggiungere alla pagina un’immagine, presa anch’essa da un data.*/
+
+const app = new Vue(
+    {
+        el: '#app',
+        data: {
+            title: 'Lorem Picsum',
+            image: 'https://picsum.photos/600/500'
+        }
+    });
